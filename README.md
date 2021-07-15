32689ba65d6551dae6ca82a17ff7d572538977fb
Peter Georgeson <peter@supernifty.org>
2021-07-16 08:45:34 +1000

    tool to generate author affiliations from table

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-## How to use
+## Abbreviation Checker
 
 ### Make an abbreviations file
 Of the form:
@@ -14,9 +14,21 @@ e.g. copy and paste to a text file
 ```
 python writing_util/abbreviations.py --abbreviations abbrev.txt < doc.txt | column -t -s'^I'
 ```
-
 ### Interpret
 Check abbreviations with an "issue". 
 
-## TODO
+### TODO
 * currently the software doesn't check across lines
+
+# Author Affiliations
+
+authors.csv is a CSV file with author name and affiliations. Specify these on the command line to generate a word doc.
+
+```
+ python ../../../util/author_affiliations.py \
+  --verbose \
+  --file authors.csv \
+  --name 'Full name' \
+  --affiliations 'Affiliation 1' 'Affiliation 2' 'Affiliation 3' 'Affiliation 4' 'Affiliation 5' 'Affiliation 6' \
+  --title 'Title of paper'
+```
---
+++ writing_util/author_affiliations.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+'''
+  word doc of author list from spreadsheet
+
+  installation:
+    pip install python-docx
+'''
+
+import argparse
+import csv
+import logging
+import sys
+
+from docx import Document
+
+def main(fn, name, affiliations, title):
+
+  document = Document()
+  p = document.add_paragraph(title).bold = True
+
+  logging.info('reading from %s...', fn)
+  affs = {} # all affiliations
+  affs_seen = set()
+  next_aff = 1
+  
+  p = document.add_paragraph()
+
+  for i, row in enumerate(csv.DictReader(open(fn, mode='r', encoding='utf-8-sig'))):
+    if i < 2:
+      logging.debug(row)
+
+    if i > 0:
+      p.add_run(', ').italic = True
+
+    n = row[name] # name of author
+    n_affs = set() # author affiliations
+    for affiliation in affiliations:
+      a = row[affiliation]
+      n_affs.add(next_aff)
+
+      if a not in affs_seen:
+        affs[next_aff] = a
+        next_aff += 1
+        affs_seen.add(a)
+
+    # write the author
+    p.add_run(n).italic = True
+    
+    # author afiliations
+    aff_txt = ','.join(sorted([str(x) for x in n_affs]))
+    run = p.add_run(aff_txt)
+    run.italic = True
+    run.font.superscript = True
+
+  # now write all the affs
+  p = document.add_paragraph()
+
+  for i in range(1, next_aff):
+    if i > 1:
+      p.add_run('; ').italic = True
+
+    num = p.add_run(str(i))
+    num.font.superscript = True
+    num.italic = True
+
+    text = affs[i]
+    p.add_run(text).italic = True
+
+  document.save('data.docx')
+
+  logging.info('done')
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Assess MSI')
+  parser.add_argument('--file', required=True, help='filename')
+  parser.add_argument('--name', required=True, help='name field')
+  parser.add_argument('--title', required=True, help='title')
+  parser.add_argument('--affiliations', nargs='+', required=True, help='affiliations fields')
+  parser.add_argument('--verbose', action='store_true', help='more logging')
+  args = parser.parse_args()
+  if args.verbose:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+  else:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+  main(args.file, args.name, args.affiliations, args.title)
